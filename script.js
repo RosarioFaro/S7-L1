0aef255815bbd4b20f03467052e5f6d8d1352f72
RosarioFaro <farorosario134@gmail.com>
2025-01-20 18:01:35 +0100

    Aggiornamento Es.2

--- script.js
+++ script.js
@@ -26,3 +26,29 @@ const user3 = new User("Renato", "Pozzetto", 84, "Milano");
 console.log(user1.compareAge(user2));
 console.log(user2.compareAge(user3));
 console.log(user1.compareAge(user3));
+
+console.log("~. ~ . ~ . ~ . Esercizio 2 . ~ . ~ . ~ . ~");
+
+const pets = [];
+
+class Pet {
+  constructor(name, owner, species, breed) {
+    this.petName = name;
+    this.ownerName = owner;
+    this.species = species;
+    this.breed = breed;
+  }
+}
+
+const formShop = document.getElementById("formShop");
+formShop.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const nameInput = document.getElementById("name");
+  const ownerInput = document.getElementById("ownerName");
+  const speciesInput = document.getElementById("species");
+  const breedInput = document.getElementById("breed");
+  const newPet = new Pet(nameInput.value, ownerInput.value, speciesInput.value, breedInput.value);
+  pets.push(newPet);
+  console.log(newPet);
+  console.log(pets);
+});
